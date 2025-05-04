ec52fe8a90a312c17cb4353f9e538ddaf897fd09
mttspss <matteo.spissu7@gmail.com>
2025-05-04 16:39:56 +0200

    Add ProgressBar above post content

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { getPostData, getAllPosts } from '@/lib/posts';
+import ProgressBar from '@/components/ProgressBar';
 
 export async function generateStaticParams() {
   return getAllPosts().map(post => ({ id: post.id }));
@@ -8,13 +9,20 @@ export async function generateStaticParams() {
 export default async function Page(context: any) {
   const { id } = context.params;
   const postData = await getPostData(id);
+  const { date } = postData;
 
   return (
-    <article className="prose prose-lg mx-auto py-8">
-      <h1>{postData.title}</h1>
-      <p>{postData.date}</p>
-      <div dangerouslySetInnerHTML={{ __html: postData.content }} />
-    </article>
+    <>
+      <ProgressBar
+        date={date}
+        startDate="2025-05-01"
+        totalDays={365}
+      />
+      <article className="prose prose-lg mx-auto py-8">
+        <h1>{postData.title}</h1>
+        <p>{date}</p>
+        <div dangerouslySetInnerHTML={{ __html: postData.content }} />
+      </article>
+    </>
   );
 }
-
